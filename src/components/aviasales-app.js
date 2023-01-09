f6e3ee3cce45f84e8603ba22887189a0efe8916a
Xardas <vadikradik@yandex.ru>
2023-01-09 12:49:06 +0300

    initial commit

---
+++ src/components/aviasales-app.js
@@ -0,0 +1,11 @@
+import './aviasales-app.css'
+
+function AviasalesApp() {
+  return (
+    <div className="aviasales-app">
+      <h1>AviasalesApp</h1>
+    </div>
+  )
+}
+
+export default AviasalesApp
