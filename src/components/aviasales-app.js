3b22d4a0b245b268c5f5036be339fb3979958a44
Xardas <vadikradik@yandex.ru>
2023-01-09 13:16:44 +0300

    scss added

--- src/components/aviasales-app.js
+++ src/components/aviasales-app.js
@@ -1,8 +1,9 @@
-import './aviasales-app.css'
+import classes from './aviasales-app.module.scss'
 
 function AviasalesApp() {
+  console.log(classes)
   return (
-    <div className="aviasales-app">
+    <div className={classes['aviasales-app']}>
       <h1>AviasalesApp</h1>
     </div>
   )
