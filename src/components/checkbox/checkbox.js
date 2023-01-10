a679c13d3d1808b4239a6637bcd0ca092834fd8c
Xardas <vadikradik@yandex.ru>
2023-01-10 16:05:11 +0300

    transfers-filter styles

---
+++ src/components/checkbox/checkbox.js
@@ -0,0 +1,20 @@
+import classnames from 'classnames'
+
+import classes from './checkbox.module.scss'
+
+const Checkbox = (props) => {
+  const labelClass = classnames(classes['checkbox'], {
+    [classes['checkbox--focused']]: props.focused,
+  })
+  const checkboxClass = classnames(classes['checkbox--visually-hidden'])
+  const checkboxViewClass = classnames(classes['checkbox__view'])
+  return (
+    <label className={labelClass}>
+      <input type="checkbox" className={checkboxClass} checked={props.checked} />
+      <span className={checkboxViewClass}></span>
+      {props.label}
+    </label>
+  )
+}
+
+export default Checkbox
--- src/components/transfers-filter/transfers-filter.js
+++ src/components/transfers-filter/transfers-filter.js
@@ -1,7 +1,30 @@
+import Checkbox from '../checkbox/checkbox'
+
 import classes from './transfers-filter.module.scss'
 
 const TransfersFilter = (props) => {
-  return <div className={`${classes['transfers-filter']} ${props.className}`}></div>
+  return (
+    <div className={`${classes['transfers-filter']} ${props.className}`}>
+      <div className={classes['transfers-filter__list']}>
+        <span className={classes['transfers-filter__list-label']}>Количество пересадок</span>
+        <div className={classes['transfers-filter__list-item']}>
+          <Checkbox label="Все" focused />
+        </div>
+        <div className={`${classes['transfers-filter__list-item']} ${classes['transfers-filter__list-item--focused']}`}>
+          <Checkbox label="Без пересадок" focused />
+        </div>
+        <div className={classes['transfers-filter__list-item']}>
+          <Checkbox label="1 пересадка" checked />
+        </div>
+        <div className={classes['transfers-filter__list-item']}>
+          <Checkbox label="2 пересадки" focused />
+        </div>
+        <div className={classes['transfers-filter__list-item']}>
+          <Checkbox label="3 пересадки" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default TransfersFilter
