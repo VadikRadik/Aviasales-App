a39bf47c3952e82e2fd06b2f3ce9062dba92b70f
Xardas <vadikradik@yandex.ru>
2023-01-10 17:10:14 +0300

    optimal-filter styles

--- src/components/more-button/more-button.js
+++ src/components/more-button/more-button.js
@@ -1,7 +1,7 @@
 import classes from './more-button.module.scss'
 
 const MoreButton = (props) => {
-  return <div className={`${classes['more-button']} ${props.className}`}>Показать ещё 5 билетов!</div>
+  return <button className={`${classes['more-button']} ${props.className}`}>Показать ещё 5 билетов!</button>
 }
 
 export default MoreButton
--- src/components/optimal-filter/optimal-filter.js
+++ src/components/optimal-filter/optimal-filter.js
@@ -1,7 +1,15 @@
 import classes from './optimal-filter.module.scss'
 
 const OptimalFilter = () => {
-  return <div className={classes['optimal-filter']}></div>
+  return (
+    <div className={classes['optimal-filter']}>
+      <div className={`${classes['optimal-filter__item']} ${classes['optimal-filter__item--selected']}`}>
+        Самый дешевый
+      </div>
+      <div className={classes['optimal-filter__item']}>Самый быстрый</div>
+      <div className={classes['optimal-filter__item']}>Оптимальный</div>
+    </div>
+  )
 }
 
 export default OptimalFilter
